7c3fbb5a5ab25849aabfe604a30f272848df1119
Daryl Watson <darylwtsn@gmail.com>
2015-06-15 14:38:09 +0100

    We are awesome bitches

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -16,7 +16,12 @@ describe Restaurant, type: :model do
   it 'is not valid with a ame of less than three characters' do
     restaurant = Restaurant.new(name: "kf")
     expect(restaurant).to have(1).error_on(:name)
-    expect(restaurant).not_to_be_valid
+    expect(restaurant).not_to be_valid
   end
 
+  it 'is not valid unless it had a unique name' do
+    Restaurant.create(name: "Moe's Tavern")
+    restaurant = Restaurant.new(name: "Moe's Tavern")
+    expect(restaurant).to have(1).error_on(:name)
+  end
 end
