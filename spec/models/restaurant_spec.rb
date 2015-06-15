170cdde2e073443628bc60f947b3dfd0fbd260a9
Daryl Watson <darylwtsn@gmail.com>
2015-06-15 13:56:32 +0100

    Unit tested review destruction

--- spec/models/restaurant_spec.rb
+++ spec/models/restaurant_spec.rb
@@ -3,4 +3,14 @@ require 'spec_helper'
 
 describe Restaurant, type: :model do
   it { is_expected.to have_many :reviews}
+
+  context 'when deleted' do
+    it 'all information is deleted' do
+      trade = Restaurant.create(name: 'Trade')
+      Review.create(thoughts: 'Awesome', rating: '5', restaurant_id: trade.id)
+      trade.destroy
+      expect(Review.count).to eq 0
+    end
+  end
+
 end
